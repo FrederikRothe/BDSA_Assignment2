dfec0e74374eba4ac51cf4d5b07c290c17a72648
Frederik Avlund <favl@itu.dk>
2021-09-20 13:53:02 +0200

    ImmutableStudent record and tests implemented

--- MyApp/MyApp.Tests/UnitTest1.cs
+++ MyApp/MyApp.Tests/UnitTest1.cs
@@ -6,15 +6,54 @@ namespace MyApp.Tests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void CreateStudentOutputToStringFormat()
         {
             Student s = new Student(200,"John","Doe",enrollmentStatus.Active, new DateTime(2020,1,1),new DateTime(2023,1,1),new DateTime());
             
-            var expected = "#" + s.id + " - " + s.givenName + " " + s.surname + " " + s.status + " " + s.startDate + " - " + s.endDate;
-            
+            var expected = "#200 - John Doe Active 01.01.2020 00.00.00 - 01.01.2023 00.00.00";
+
             var actual  = s.toString();
+            Console.WriteLine(actual);
 
             Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void CreateTwoImmutableStudentAndCheckIfEqual_True()
+        {
+            ImmutableStudent first = new ImmutableStudent(200,"John","Doe",enrollmentStatus.Active, new DateTime(2020,1,1),new DateTime(2023,1,1),new DateTime());
+            
+            ImmutableStudent second = new ImmutableStudent(200,"John","Doe",enrollmentStatus.Active, new DateTime(2020,1,1),new DateTime(2023,1,1),new DateTime());
+
+            Assert.True(first == second);
+        }
+
+        [Fact]
+        public void CreateImmutableStudentAndCheckToString()
+        {
+            ImmutableStudent first = new ImmutableStudent(200,"John","Doe",enrollmentStatus.Active, new DateTime(2020,1,1),new DateTime(2023,1,1),new DateTime());
+            
+            var expected = "#200 - John Doe Active 01.01.2020 00.00.00 - 01.01.2023 00.00.00";
+
+            var actual  = first.toString();
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void CompareImmutableStudentAndStudentToString()
+        {
+            Student first = new Student(200,"John","Doe",enrollmentStatus.Active, new DateTime(2020,1,1),new DateTime(2023,1,1),new DateTime());
+            
+            ImmutableStudent second = new ImmutableStudent(200,"John","Doe",enrollmentStatus.Active, new DateTime(2020,1,1),new DateTime(2023,1,1),new DateTime());
+            
+            var expected = "#200 - John Doe Active 01.01.2020 00.00.00 - 01.01.2023 00.00.00";
+            
+            var actualFromFirst  = first.toString();
+            var actualFromSecond  = second.toString();
+
+            Assert.Equal(expected, actualFromFirst);
+            Assert.Equal(expected, actualFromSecond);
         }
     }
 }
