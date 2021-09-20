598647507e95197a36ab5664211a2ff2aeb8387c
Jacob Møller <jacj@itu.dk>
2021-09-20 14:43:49 +0200

    Immutable student (#3)
    * Initial commit
    
    * Initial solution file, folders and unit test.
    
    * github workflows added
    
    * Inserted fields in student.
    
    * testing student
    
    * Filepath addition to yaml file
    
    * Implemented toString() and Student class
    
    * ImmutableStudent record and tests implemented
    
    Co-authored-by: Frakty <66801011+FrederikRothe@users.noreply.github.com>
    Co-authored-by: Frederik Avlund <favl@itu.dk>

--- MyApp/MyApp/Student.cs
+++ MyApp/MyApp/Student.cs
@@ -26,6 +26,31 @@ namespace MyApp
             return "#" + id + " - " + givenName + " " + surname + " " + status + " " + startDate + " - " + endDate;
         }
     }
+
+    public record ImmutableStudent
+    {
+        public int id {get; init;}
+        public string givenName {get; init;}
+        public string surname {get; init;}
+        public enrollmentStatus status {get; init;}
+        public DateTime startDate {get; init;}
+        public DateTime endDate {get; init;}
+        public DateTime graduationDate {get; init;}
+
+        public ImmutableStudent(int id, string givenName, string surname, enrollmentStatus status, DateTime startDate, DateTime endDate, DateTime graduationDate) {
+            this.id  = id;
+            this.givenName = givenName;
+            this.surname = surname;
+            this.status = status;
+            this.startDate = startDate;
+            this.endDate = endDate;
+            this.graduationDate = graduationDate;     
+        }
+
+        public string toString() {
+            return "#" + id + " - " + givenName + " " + surname + " " + status + " " + startDate + " - " + endDate;
+        }
+    }
 }
 
 public enum enrollmentStatus
