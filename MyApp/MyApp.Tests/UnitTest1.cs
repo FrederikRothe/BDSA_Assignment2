8c0e49d20663e67bc3e63a4e614eb7d8597b95df
JacobM <jacj@itu.dk>
2021-09-23 11:49:40 +0200

    EnrollmentStatusSelector method and tests to cover this

--- MyApp/MyApp.Tests/UnitTest1.cs
+++ MyApp/MyApp.Tests/UnitTest1.cs
@@ -8,12 +8,47 @@ namespace MyApp.Tests
         [Fact]
         public void CreateStudentOutputToStringFormat()
         {
-            Student s = new Student(200,"John","Doe",enrollmentStatus.Active, new DateTime(2020,1,1),new DateTime(2023,1,1),new DateTime());
-            
+            Student s = new Student(200, "John", "Doe", new DateTime(2020, 1, 1), new DateTime(2023, 1, 1), new DateTime(2023, 1, 1));
+
             var expected = "#200 - John Doe Active 01.01.2020 00.00.00 - 01.01.2023 00.00.00";
 
-            var actual  = s.toString();
-            Console.WriteLine(actual);
+            var actual = s.toString();
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void CreateStudentAndCheckEnrollmentStatus_New()
+        {
+            Student s = new Student(200, "John", "Doe", DateTime.Now.AddDays(-10), new DateTime(2023, 1, 1), new DateTime(2023, 1, 1));
+
+            var expected = "#200 - John Doe New " + DateTime.Now.AddDays(-10) + " - 01.01.2023 00.00.00";
+
+            var actual = s.toString();
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void CreateStudentAndCheckEnrollmentStatus_Dropout()
+        {
+            Student s = new Student(200, "John", "Doe", new DateTime(2020, 1, 1), DateTime.Now.AddDays(-10), new DateTime(2023, 1, 1));
+
+            var expected = "#200 - John Doe Dropout 01.01.2020 00.00.00 - " + DateTime.Now.AddDays(-10);
+
+            var actual = s.toString();
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void CreateStudentAndCheckEnrollmentStatus_Graduated()
+        {
+            Student s = new Student(200, "John", "Doe", new DateTime(2020, 1, 1), new DateTime(2023, 1, 1), DateTime.Now.AddDays(-10));
+
+            var expected = "#200 - John Doe Graduated 01.01.2020 00.00.00 - 01.01.2023 00.00.00";
+
+            var actual = s.toString();
 
             Assert.Equal(expected, actual);
         }
@@ -21,9 +56,9 @@ namespace MyApp.Tests
         [Fact]
         public void CreateTwoImmutableStudentAndCheckIfEqual_True()
         {
-            ImmutableStudent first = new ImmutableStudent(200,"John","Doe",enrollmentStatus.Active, new DateTime(2020,1,1),new DateTime(2023,1,1),new DateTime());
-            
-            ImmutableStudent second = new ImmutableStudent(200,"John","Doe",enrollmentStatus.Active, new DateTime(2020,1,1),new DateTime(2023,1,1),new DateTime());
+            ImmutableStudent first = new ImmutableStudent(200, "John", "Doe", new DateTime(2020, 1, 1), new DateTime(2023, 1, 1), new DateTime(2023, 1, 1));
+
+            ImmutableStudent second = new ImmutableStudent(200, "John", "Doe", new DateTime(2020, 1, 1), new DateTime(2023, 1, 1), new DateTime(2023, 1, 1));
 
             Assert.True(first == second);
         }
@@ -31,11 +66,11 @@ namespace MyApp.Tests
         [Fact]
         public void CreateImmutableStudentAndCheckToString()
         {
-            ImmutableStudent first = new ImmutableStudent(200,"John","Doe",enrollmentStatus.Active, new DateTime(2020,1,1),new DateTime(2023,1,1),new DateTime());
-            
+            ImmutableStudent first = new ImmutableStudent(200, "John", "Doe", new DateTime(2020, 1, 1), new DateTime(2023, 1, 1), new DateTime(2023, 1, 1));
+
             var expected = "#200 - John Doe Active 01.01.2020 00.00.00 - 01.01.2023 00.00.00";
 
-            var actual  = first.toString();
+            var actual = first.toString();
 
             Assert.Equal(expected, actual);
         }
@@ -43,14 +78,14 @@ namespace MyApp.Tests
         [Fact]
         public void CompareImmutableStudentAndStudentToString()
         {
-            Student first = new Student(200,"John","Doe",enrollmentStatus.Active, new DateTime(2020,1,1),new DateTime(2023,1,1),new DateTime());
-            
-            ImmutableStudent second = new ImmutableStudent(200,"John","Doe",enrollmentStatus.Active, new DateTime(2020,1,1),new DateTime(2023,1,1),new DateTime());
-            
+            Student first = new Student(200, "John", "Doe", new DateTime(2020, 1, 1), new DateTime(2023, 1, 1), new DateTime(2023, 1, 1));
+
+            ImmutableStudent second = new ImmutableStudent(200, "John", "Doe", new DateTime(2020, 1, 1), new DateTime(2023, 1, 1), new DateTime(2023, 1, 1));
+
             var expected = "#200 - John Doe Active 01.01.2020 00.00.00 - 01.01.2023 00.00.00";
-            
-            var actualFromFirst  = first.toString();
-            var actualFromSecond  = second.toString();
+
+            var actualFromFirst = first.toString();
+            var actualFromSecond = second.toString();
 
             Assert.Equal(expected, actualFromFirst);
             Assert.Equal(expected, actualFromSecond);
--- MyApp/MyApp/Student.cs
+++ MyApp/MyApp/Student.cs
@@ -4,55 +4,98 @@ namespace MyApp
 {
     public class Student
     {
-        public int id {get; init;}
-        public string givenName {get; set;}
-        public string surname {get; set;}
-        public enrollmentStatus status {get; init;}
-        public DateTime startDate {get; set;}
-        public DateTime endDate {get; set;}
-        public DateTime graduationDate {get; set;}
+        public int id { get; init; }
+        public string givenName { get; set; }
+        public string surname { get; set; }
+        public enrollmentStatus status { get; init; }
+        public DateTime startDate { get; set; }
+        public DateTime endDate { get; set; }
+        public DateTime graduationDate { get; set; }
 
-        public Student(int id, string givenName, string surname, enrollmentStatus status, DateTime startDate, DateTime endDate, DateTime graduationDate) {
-            this.id  = id;
+        public Student(int id, string givenName, string surname, DateTime startDate, DateTime endDate, DateTime graduationDate)
+        {
+            this.id = id;
             this.givenName = givenName;
             this.surname = surname;
-            this.status = status;
+            this.status = selectStatus(startDate, endDate, graduationDate);
             this.startDate = startDate;
             this.endDate = endDate;
-            this.graduationDate = graduationDate;     
+            this.graduationDate = graduationDate;
         }
-        
-        public string toString() {
+
+        public string toString()
+        {
             return "#" + id + " - " + givenName + " " + surname + " " + status + " " + startDate + " - " + endDate;
+        }
+
+        public enrollmentStatus selectStatus(DateTime startDate, DateTime endDate, DateTime graduationDate)
+        {
+            if (graduationDate < DateTime.Now)
+            {
+                return enrollmentStatus.Graduated;
+            }
+            else if (endDate < DateTime.Now)
+            {
+                return enrollmentStatus.Dropout;
+            }
+            else if (startDate >= DateTime.Now.AddDays(-14))
+            {
+                return enrollmentStatus.New;
+            }
+            else
+            {
+                return enrollmentStatus.Active;
+            }
         }
     }
 
     public record ImmutableStudent
     {
-        public int id {get; init;}
-        public string givenName {get; init;}
-        public string surname {get; init;}
-        public enrollmentStatus status {get; init;}
-        public DateTime startDate {get; init;}
-        public DateTime endDate {get; init;}
-        public DateTime graduationDate {get; init;}
+        public int id { get; init; }
+        public string givenName { get; init; }
+        public string surname { get; init; }
+        public enrollmentStatus status { get; init; }
+        public DateTime startDate { get; init; }
+        public DateTime endDate { get; init; }
+        public DateTime graduationDate { get; init; }
 
-        public ImmutableStudent(int id, string givenName, string surname, enrollmentStatus status, DateTime startDate, DateTime endDate, DateTime graduationDate) {
-            this.id  = id;
+        public ImmutableStudent(int id, string givenName, string surname, DateTime startDate, DateTime endDate, DateTime graduationDate)
+        {
+            this.id = id;
             this.givenName = givenName;
             this.surname = surname;
-            this.status = status;
+            this.status = selectStatus(startDate, endDate, graduationDate);
             this.startDate = startDate;
             this.endDate = endDate;
-            this.graduationDate = graduationDate;     
+            this.graduationDate = graduationDate;
         }
 
-        public string toString() {
+        public string toString()
+        {
             return "#" + id + " - " + givenName + " " + surname + " " + status + " " + startDate + " - " + endDate;
+        }
+
+        public enrollmentStatus selectStatus(DateTime startDate, DateTime endDate, DateTime graduationDate)
+        {
+            if (graduationDate < DateTime.Now)
+            {
+                return enrollmentStatus.Graduated;
+            }
+            else if (endDate < DateTime.Now)
+            {
+                return enrollmentStatus.Dropout;
+            }
+            else if (startDate >= DateTime.Now.AddDays(-14))
+            {
+                return enrollmentStatus.New;
+            }
+            else
+            {
+                return enrollmentStatus.Active;
+            }
         }
     }
 }
-
 public enum enrollmentStatus
 {
     New,
